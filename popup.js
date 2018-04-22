b9d3381387b01e1a7211aaad89cfba207574cf47
Joey <jyudel@gmail.com>
2018-04-22 10:38:23 -0400

    Basic popup

--- popup.js
+++ popup.js
@@ -1,1 +1,11 @@
-#do this
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+
+    // our logic
+    });
+  }, false);
+}, false);
